93a6daafdd53033f4ac740fa7fadeb4efcb19bbe
Gwee Per Ming <91836754+pmgwee@users.noreply.github.com>
2022-12-10 20:39:12 +0800

    Landing page with index.php
    Landing page for web application by writing html,css inside php

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CAT201 Assignment Excel to CSV Converter</title>
+    <link rel="stylesheet" href="css/Landing.css">
+</head>
+    <body>
+        <article>
+            <div class="itemWrapper">
+                <div class="itemBox">
+                    <div class="Title">
+                        <h1>Excel to CSV Converter</h1>
+                        <div class="marquee">
+                            <div class="marqueeInner">
+                                <span>
+                                    <h3 class="scrollingText">Convert file from excel to CSV format online for free</h3>
+                                </span>
+                                <span>
+                                    <h3 class="scrollingText">Convert file from excel to CSV format online for free</h3>
+                                </span>
+                                <span>
+                                    <h3 class="scrollingText">Convert file from excel to CSV format online for free</h3>
+                                </span>
+                                <span>
+                                    <h3 class="scrollingText">Convert file from excel to CSV format online for free</h3>
+                                </span>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="uploadContainer">
+                        <form action = "downloadPage.php" method = "post" enctype="multipart/form-data">
+                            <div class="Card">
+                                <img src="Public/selectFile.png" alt="fileIcon" width="313" height="313">
+                                <input type="file" name="userFile"    />
+                            </div>
+                            <div class="Submit">
+                                <input type ="submit"  value="Upload Excel" name="submit" >
+                            </div>
+                        </form>
+                    </div>
+                    <div class="descContainer">
+                        <div class="step1">
+                            <div class="imgBox" >
+                                <img src="Public/fileIcon.png" alt="fileIcon" width="113" height="113">
+                            </div>
+                            <p class="underline-text-animation">Select an excel file from your computer</p>
+                        </div>
+                        <hr>
+                        <div class="step2">
+                            <div class="imgBox">
+                                <img src="Public/convertIcon.png" alt="convertIcon" width="113" height="113">
+                            </div>
+
+                            <p class="underline-text-animation">Our system will automatically help you to convert the filetype to CSV</p>
+                        </div>
+                        <hr>
+                        <div class="step3">
+                            <div class="imgBox">
+                                <img src="Public/downloadIcon.png" alt="downloadIcon" width="113" height="113">
+                            </div>
+                            <p class="underline-text-animation">Download your converted file immediately</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </article>
+    </body>
+</html>
